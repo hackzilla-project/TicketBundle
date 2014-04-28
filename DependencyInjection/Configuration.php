fb15021161f87a3e37b88962a1cd79fef0470f95
Daniel Platt <github@ofdan.co.uk>
2014-04-28 22:09:27 +0100

    add root node back

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,6 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('hackzilla_ticket');
 
         return $treeBuilder;
     }
