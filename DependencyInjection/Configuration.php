136f3657acd9bf204b17b1f1be7850d04cafb842
Daniel Platt <github@ofdan.co.uk>
2014-04-28 21:17:25 +0100

    remove unused variables and code

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hackzilla_ticket');
 
         return $treeBuilder;
     }
--- DependencyInjection/HackzillaTicketExtension.php
+++ DependencyInjection/HackzillaTicketExtension.php
@@ -14,15 +14,17 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class HackzillaTicketExtension extends Extension
 {
+
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
+
 }
