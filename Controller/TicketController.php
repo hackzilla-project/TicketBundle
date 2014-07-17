1c8cbc9ef8c9b8fec6ec51c8aa3ed2d88644701b
Daniel Platt <github@ofdan.co.uk>
2014-07-17 21:39:55 +0100

    Fixes #11. remove unused use statement.

--- Controller/TicketController.php
+++ Controller/TicketController.php
@@ -4,7 +4,6 @@ namespace Hackzilla\Bundle\TicketBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Hackzilla\Bundle\TicketBundle\Entity\Ticket;
 use Hackzilla\Bundle\TicketBundle\Entity\TicketMessage;
 use Hackzilla\Bundle\TicketBundle\Form\TicketType;
