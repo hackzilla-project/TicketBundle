5b6d00ee0555f808578bc7cad5782f87d587afc8
Daniel Platt <github@ofdan.co.uk>
2016-01-16 22:12:03 +0000

    add missing use

--- Form/Type/PriorityType.php
+++ Form/Type/PriorityType.php
@@ -4,6 +4,7 @@ namespace Hackzilla\Bundle\TicketBundle\Form\Type;
 
 use Hackzilla\Bundle\TicketBundle\Entity\TicketMessage;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PriorityType extends AbstractType
--- Tests/Form/Type/TicketMessageTypeTest.php
+++ Tests/Form/Type/TicketMessageTypeTest.php
@@ -5,6 +5,7 @@ namespace Hackzilla\Bundle\TicketBundle\Tests\Form\Type;
 use Hackzilla\Bundle\TicketBundle\Entity\TicketMessage;
 use Hackzilla\Bundle\TicketBundle\Form\Type\TicketMessageType;
 use Hackzilla\Bundle\TicketBundle\User\UserInterface;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\PreloadedExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
 
