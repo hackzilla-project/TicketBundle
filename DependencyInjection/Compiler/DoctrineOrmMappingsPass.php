445dbde34608432f3317a30be5d52eedbcf540f4
Daniel Platt <github@ofdan.co.uk>
2016-06-23 19:44:17 +0100

    handle paths in custom compiler pass

---
+++ DependencyInjection/Compiler/DoctrineOrmMappingsPass.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hackzilla\Bundle\TicketBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class DoctrineOrmMappingsPass extends \Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass
+{
+    function __construct($driver = null, array $namespaces = [], $managerParameters = [], $enabledParameter = false, array $aliasMap = [])
+    {
+        parent::__construct($driver, $namespaces, $managerParameters, $enabledParameter, $aliasMap);
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $namespaces = [];
+
+        if (
+            $container->getParameter('hackzilla_ticket.model.ticket.class') === 'Hackzilla\Bundle\TicketBundle\Entity\Ticket'
+        ) {
+            $path = realpath(__DIR__.'/../../Resources/config/doctrine/model/Ticket');
+            $namespaces[$path] = 'Hackzilla\Bundle\TicketBundle\Entity';
+        }
+
+        if (
+            $container->getParameter('hackzilla_ticket.model.message.class') === 'Hackzilla\Bundle\TicketBundle\Entity\TicketMessage'
+        ) {
+            $path = realpath(__DIR__.'/../../Resources/config/doctrine/model/TicketMessage');
+            $namespaces[$path] = 'Hackzilla\Bundle\TicketBundle\Entity';
+        }
+
+        $arguments = array($namespaces, '.orm.xml');
+        $locator = new Definition('Doctrine\Common\Persistence\Mapping\Driver\SymfonyFileLocator', $arguments);
+        $this->driver = new Definition('Doctrine\ORM\Mapping\Driver\XmlDriver', array($locator));
+        $this->namespaces = $namespaces;
+
+        parent::process($container);
+    }
+}
--- HackzillaTicketBundle.php
+++ HackzillaTicketBundle.php
@@ -2,7 +2,7 @@
 
 namespace Hackzilla\Bundle\TicketBundle;
 
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Hackzilla\Bundle\TicketBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,38 +15,8 @@ class HackzillaTicketBundle extends Bundle
     {
         parent::build($container);
 
-        $this->buildOrmCompilerPass($container);
-    }
-
-    /**
-     * Creates and registers compiler passes for ORM mappings.
-     *
-     * @param ContainerBuilder $container
-     */
-    private function buildOrmCompilerPass(ContainerBuilder $container)
-    {
-        if (!class_exists('Doctrine\ORM\Version')) {
-            return;
-        }
-
-        $entities = [];
-
-//        if (
-//            $container->hasParameter('hackzilla_ticket.model.ticket.class')
-//            &&
-//            $container->getParameter('hackzilla_ticket.model.ticket.class') === 'Hackzilla\Bundle\TicketBundle\Entity\Ticket'
-//        ) {
-            $entities[realpath(__DIR__.'/Resources/config/doctrine/model/Ticket')] = 'Hackzilla\Bundle\TicketBundle\Entity';
-//        }
-
-//        if ($container->getParameter('hackzilla_ticket.model.message.class') === 'Hackzilla\Bundle\TicketBundle\Entity\TicketMessage') {
-            $entities[realpath(__DIR__.'/Resources/config/doctrine/model/TicketMessage')] = 'Hackzilla\Bundle\TicketBundle\Entity';
-//        }
-
-        if ($entities) {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createXmlMappingDriver($entities)
-            );
-        }
+        $container->addCompilerPass(
+            new DoctrineOrmMappingsPass()
+        );
     }
 }
