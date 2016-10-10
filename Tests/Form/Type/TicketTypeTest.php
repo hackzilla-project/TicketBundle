bcd122651657d2c48cee7a980eb1488b082e6890
Михаил <mb@kelnik.ru>
2016-10-10 16:46:11 +0300

    Fix TicketTypeTest, use getMockBuilder() to get the mock

--- Tests/Form/Type/TicketTypeTest.php
+++ Tests/Form/Type/TicketTypeTest.php
@@ -17,7 +17,7 @@ class TicketTypeTest extends TypeTestCase
 
     protected function setUp()
     {
-        $this->user = $this->createMock(UserManagerInterface::class);
+        $this->user = $this->getMockBuilder(UserManagerInterface::class)->getMock();
 
         parent::setUp();
     }
